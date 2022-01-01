cebf9782d0fb35a66942849505e90c6051115d9e
Wraient Daemon <81632222+Wraient@users.noreply.github.com>
2022-01-01 16:53:35 +0530

    Update digiboard_change_wallpaper_to_windoge_10_and_start_rickroll_video.ino
    This Digispark script works fine on windows 10 and windows 7 with a net runtime of 15 seconds as it needs to configure things before running the script it would take upto 5 seconds to pre-configure and then 10 seconds to run the script. I have tested this is windows 10 64-bit [8GB DDR3 ram core i3 8th gen on HDD] and windows 10 32-bit [4gb DDR4 ram core i3 8th gen on HDD] it works perfectly on both. But on windows 7 it might not change the background and needs to increase the delay between two commands.
    Disclaimer :  This script only run in windows as it uses run program which is in-build in windows. You can make the same script for linux by using terminal instead of run program. 
    The code in this file is been written by me and you are free to use it as you want. you might want to increase the delay if you want to use this script for low-end PC or laptop.
    If you find any bug or want to make this script more efficient then please reach out to me on Instagram at @wraient.911 , your help will be appreciated.

--- digiboard_change_wallpaper_to_windoge_10_and_start_rickroll_video.ino
+++ digiboard_change_wallpaper_to_windoge_10_and_start_rickroll_video.ino
@@ -1,39 +1,26 @@
 #include "DigiKeyboard.h"
 void setup() {
-  //empty
-}
-void loop() {
-  // the following will change your wallpaper to doge theme or any link to image you put in the 18th line
-  DigiKeyboard.sendKeyStroke(0);
+  // The following will change your wallpaper to doge theme or any link to image you put in 11h line
   DigiKeyboard.sendKeyStroke(KEY_D, MOD_GUI_LEFT);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_R,MOD_GUI_LEFT);
+  DigiKeyboard.delay(100);
   DigiKeyboard.print("powershell");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(500);
-  DigiKeyboard.print("$client = new-object System.Net.WebClient");
+  DigiKeyboard.print("$client = new-object System.Net.WebClient;$client.DownloadFile(\"https://i.imgur.com/0sFg0CS.jpg\" , \"doge.jpg\");reg add \"HKCU\\Control Panel\\Desktop\" /v WallPaper /d \"%USERPROFILE%\\doge.jpg\" /f;RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True;exit");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print("$client.DownloadFile(\"https://3.bp.blogspot.com/-mq79Qd7bGHI/V5BfVZgyt2I/AAAAAAAAAH4/1_11RUAfJowxyqoc9lpSSV6IQMGUeuksQCLcB/s1600/xz6tAT3.jpg\" , \"doge.jpg\")");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print("reg add \"HKCU\\Control Panel\\Desktop\" /v WallPaper /d \"%USERPROFILE%\\doge.jpg\" /f");
-  DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print("RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print("exit");
   // ----------------------------------------end---------------------------------------------
-  // The following will open the youtube video link on line 35 in fullscreen in default browser
+  // The following will open the youtube video link on line 17th in fullscreen on default browser
+  DigiKeyboard.sendKeyStroke(KEY_R,MOD_GUI_LEFT);
+  DigiKeyboard.delay(50);
+  DigiKeyboard.print("https://www.youtube.com/watch?v=a3Z7zEc7AXQ");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(500);
-  DigiKeyboard.print("https://www.youtube.com/watch?v=BjDebmqFRuc");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(2000);
+  DigiKeyboard.delay(5000);
   DigiKeyboard.sendKeyStroke(KEY_F);
+  // ----------------------------------------end---------------------------------------------
+}
+void loop() {
+  // You can run the upper scipt here and then it will have the same exact effect but the scipt will run again and again untill you unplug the board.
+  //empty
 }
